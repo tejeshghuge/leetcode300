fdfea8a77ad3a2ecbfc3ea2091ea6751b6f94d4b
Tejesh Ghuge <160776993+tejeshghuge@users.noreply.github.com>
2025-10-14 20:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (48.41%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -13,20 +13,44 @@
  *     }
  * }
  */
+
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-        if (key < root.val) {
-            root.left = deleteNode(root.left, key);
-        } else if (key > root.val) {
-            root.right = deleteNode(root.right, key);
-        } else {
-            if (root.left == null) return root.right;
-            if (root.right == null) return root.left;
-            TreeNode successor = root.right;
-            while (successor.left != null) successor = successor.left;
-            root.val = successor.val;
-            root.right = deleteNode(root.right, successor.val);
+        //search
+        if(root == null ) {
+            return null;
+        }
+        if(root.val<key){
+            root.right = deleteNode(root.right,key);
+        }
+        else if(root.val>key) {
+            root.left = deleteNode(root.left,key);
+        }
+        else{
+            //case 1: leaf node
+            if(root.left == null && root.right == null ) {
+                return null;
+            }
+            //case 2 : one child
+            if(root.left ==  null){
+                return root.right;
+            }
+            else if(root.right ==  null ) {
+                return root.left;
+            }
+            //case 3 : two children
+            TreeNode IS = findInOrderSuccessor(root.right);
+            root.val = IS.val;
+
+            
+            root.right = deleteNode(root.right,IS.val);
+        }
+        return root;
+
+    }
+    private TreeNode findInOrderSuccessor(TreeNode root){
+        while(root.left != null){
+            root = root.left;
         }
         return root;
     }
